8a967a83244dc308f637f0067f5c28ba35e79711
rlam4 <rlam4@umd.edu>
2015-09-16 23:49:21 -0400

    Java Files
    Java Files

---
+++ IGEDriver.java
@@ -0,0 +1,21 @@
+package iGottaEat;
+import java.util.*;
+
+
+
+
+public class IGEDriver {
+	
+	ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>(); 
+	ArrayList<User> users = new ArrayList<User>();
+	
+	public static void main(String[] args) {
+		
+		/*This is a place holder until we can get our GUI operational.*/
+		System.out.println("Welcome to the random food generator. Here are your options:");
+		System.out.println("1 - Favorite Playlists");
+		System.out.println("2 - Create Random Playlist");
+		System.out.println("3 - Popular Restaurants");
+		System.out.println("4 - Generate random restaurant");
+	}
+}
---
+++ Restaurant.java
@@ -0,0 +1,95 @@
+package iGottaEat;
+
+public class Restaurant {
+	String name, number, times, address;
+	Double distance;
+	int price;
+	int allTimeVisitors;
+	int yearVisitors;
+	int monthVisitors;
+	int weekVisitors;
+	int dayVisitors;
+	
+	/*getters and setters*/
+	
+	private String getName() {
+		return name;
+	}
+	private void setName(String name) {
+		this.name = name;
+	}
+	
+	private String getNumber() {
+		return number;
+	}
+	private void setNumber(String number) {
+		this.number = number;
+	}
+	
+	private String getTimes() {
+		return times;
+	}
+	private void setTimes(String times) {
+		this.times = times;
+	}
+	
+	private String getAddress() {
+		return address;
+	}
+	private void setAddress(String address) {
+		this.address = address;
+	}
+	
+	private Double getDistance() {
+		return distance;
+	}
+	private void setDistance(Double distance) {
+		this.distance = distance;
+	}
+	
+	private int getPrice() {
+		return price;
+	}
+	private void setPrice(int price) {
+		this.price = price;
+	}
+	
+	private int getAllTimeVisitors() {
+		return allTimeVisitors;
+	}
+	private void setAllTimeVisitors(int allTimeVisitors) {
+		this.allTimeVisitors = allTimeVisitors;
+	}
+	
+	private int getYearVisitors() {
+		return yearVisitors;
+	}
+	private void setYearVisitors(int yearVisitors) {
+		this.yearVisitors = yearVisitors;
+	}
+	private int getMonthVisitors() {
+		return monthVisitors;
+	}
+	
+	private void setMonthVisitors(int monthVisitors) {
+		this.monthVisitors = monthVisitors;
+	}
+	
+	private int getWeekVisitors() {
+		return weekVisitors;
+	}
+	private void setWeekVisitors(int weekVisitors) {
+		this.weekVisitors = weekVisitors;
+	}
+	
+	private int getDayVisitors() {
+		return dayVisitors;
+	}
+	private void setDayVisitors(int dayVisitors) {
+		this.dayVisitors = dayVisitors;
+	}
+	
+	
+	
+	
+}
